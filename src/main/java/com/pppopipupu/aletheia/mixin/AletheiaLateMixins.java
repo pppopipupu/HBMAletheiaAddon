520931df9341ed6fd1b54d7dd0e119578cde4a25
pppopipupu <pppopipupu@gmail.com>
2026-07-13 00:49:08 +0800

    fix moron llm forget late mixin

---
+++ src/main/java/com/pppopipupu/aletheia/mixin/AletheiaLateMixins.java
@@ -0,0 +1,16 @@
+package com.pppopipupu.aletheia.mixin;
+
+import java.util.Collections;
+import java.util.List;
+
+import io.github.tox1cozz.mixinbooterlegacy.ILateMixinLoader;
+import io.github.tox1cozz.mixinbooterlegacy.LateMixin;
+
+@LateMixin
+public class AletheiaLateMixins implements ILateMixinLoader {
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.aletheia.late.json");
+    }
+}
--- src/main/java/com/pppopipupu/aletheia/mixin/MixinMainRegistry.java
+++ src/main/java/com/pppopipupu/aletheia/mixin/MixinMainRegistry.java
@@ -1,5 +1,7 @@
 package com.pppopipupu.aletheia.mixin;
 
+import net.minecraft.item.Item;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,69 +20,115 @@ public class MixinMainRegistry {
 
     @Inject(method = "handleMissingMappings", at = @At("RETURN"))
     private void aletheia$handleMissingMappings(FMLMissingMappingsEvent event, CallbackInfo ci) {
+        MainRegistry.logger.info("Aletheia missing mappings remapper started processing.");
         for (MissingMapping mapping : event.get()) {
+            MainRegistry.logger.info("Processing missing mapping: " + mapping.name + " of type " + mapping.type);
             if (mapping.type == GameRegistry.Type.ITEM) {
                 if ("hbm:item.bucket_qgp".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.bucket_qgp);
                     mapping.remap(AletheiaItems.bucket_qgp);
                 } else if ("hbm:item.qgp_mining_bomb".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.qgp_mining_bomb);
                     mapping.remap(AletheiaItems.qgp_mining_bomb);
                 } else if ("hbm:item.upgrade_ultimate".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.upgrade_ultimate);
                     mapping.remap(AletheiaItems.upgrade_ultimate);
                 } else if ("hbm:item.gun_pppop".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.gun_pppop);
                     mapping.remap(AletheiaItems.gun_pppop);
                 } else if ("hbm:item.ams_muzzle".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.ams_muzzle);
                     mapping.remap(AletheiaItems.ams_muzzle);
                 } else if ("hbm:item.ams_focus_limiter".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.ams_focus_limiter);
                     mapping.remap(AletheiaItems.ams_focus_limiter);
                 } else if ("hbm:item.ams_focus_booster".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.ams_focus_booster);
                     mapping.remap(AletheiaItems.ams_focus_booster);
                 } else if ("hbm:item.night_vision_mk2".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.night_vision_mk2);
                     mapping.remap(AletheiaItems.night_vision_mk2);
                 } else if ("hbm:item.billet_rs1".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.billet_rs1);
                     mapping.remap(AletheiaItems.billet_rs1);
                 } else if ("hbm:item.billet_rs2".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.billet_rs2);
                     mapping.remap(AletheiaItems.billet_rs2);
                 } else if ("hbm:item.billet_rs3".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.billet_rs3);
                     mapping.remap(AletheiaItems.billet_rs3);
                 } else if ("hbm:item.rbmk_pellet_rs1".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_pellet_rs1);
                     mapping.remap(AletheiaItems.rbmk_pellet_rs1);
                 } else if ("hbm:item.rbmk_pellet_rs2".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_pellet_rs2);
                     mapping.remap(AletheiaItems.rbmk_pellet_rs2);
                 } else if ("hbm:item.rbmk_pellet_rs3".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_pellet_rs3);
                     mapping.remap(AletheiaItems.rbmk_pellet_rs3);
                 } else if ("hbm:item.rbmk_fuel_rs1".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_fuel_rs1);
                     mapping.remap(AletheiaItems.rbmk_fuel_rs1);
                 } else if ("hbm:item.rbmk_fuel_rs2".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_fuel_rs2);
                     mapping.remap(AletheiaItems.rbmk_fuel_rs2);
                 } else if ("hbm:item.rbmk_fuel_rs3".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.rbmk_fuel_rs3);
                     mapping.remap(AletheiaItems.rbmk_fuel_rs3);
                 } else if ("hbm:item.spawn_maskman".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.spawn_maskman);
                     mapping.remap(AletheiaItems.spawn_maskman);
                 } else if ("hbm:item.recipe_icon".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping item: " + mapping.name + " to " + AletheiaItems.recipe_icon);
                     mapping.remap(AletheiaItems.recipe_icon);
                 } else if ("hbm:item.block_sodium".equals(mapping.name)) {
-                    mapping.remap(AletheiaBlocks.block_sodium);
+                    Item targetItem = Item.getItemFromBlock(AletheiaBlocks.block_sodium);
+                    MainRegistry.logger.info("Remapping item block: " + mapping.name + " to " + targetItem);
+                    mapping.remap(targetItem);
                 } else if ("hbm:item.block_strontium".equals(mapping.name)) {
-                    mapping.remap(AletheiaBlocks.block_strontium);
+                    Item targetItem = Item.getItemFromBlock(AletheiaBlocks.block_strontium);
+                    MainRegistry.logger.info("Remapping item block: " + mapping.name + " to " + targetItem);
+                    mapping.remap(targetItem);
                 } else if ("hbm:item.block_neodymium".equals(mapping.name)) {
-                    mapping.remap(AletheiaBlocks.block_neodymium);
+                    Item targetItem = Item.getItemFromBlock(AletheiaBlocks.block_neodymium);
+                    MainRegistry.logger.info("Remapping item block: " + mapping.name + " to " + targetItem);
+                    mapping.remap(targetItem);
                 }
             } else if (mapping.type == GameRegistry.Type.BLOCK) {
                 if ("hbm:tile.qgp_block".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.qgp_block);
                     mapping.remap(AletheiaBlocks.qgp_block);
                 } else if ("hbm:tile.ams_base".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.ams_base);
                     mapping.remap(AletheiaBlocks.ams_base);
                 } else if ("hbm:tile.ams_emitter".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.ams_emitter);
                     mapping.remap(AletheiaBlocks.ams_emitter);
                 } else if ("hbm:tile.ams_limiter".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.ams_limiter);
                     mapping.remap(AletheiaBlocks.ams_limiter);
                 } else if ("hbm:tile.machine_schrabidium_transmutator".equals(mapping.name)) {
+                    MainRegistry.logger.info(
+                        "Remapping block: " + mapping.name + " to " + AletheiaBlocks.machine_schrabidium_transmutator);
                     mapping.remap(AletheiaBlocks.machine_schrabidium_transmutator);
                 } else if ("hbm:tile.block_sodium".equals(mapping.name)) {
+                    MainRegistry.logger.info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.block_sodium);
                     mapping.remap(AletheiaBlocks.block_sodium);
                 } else if ("hbm:tile.block_strontium".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.block_strontium);
                     mapping.remap(AletheiaBlocks.block_strontium);
                 } else if ("hbm:tile.block_neodymium".equals(mapping.name)) {
+                    MainRegistry.logger
+                        .info("Remapping block: " + mapping.name + " to " + AletheiaBlocks.block_neodymium);
                     mapping.remap(AletheiaBlocks.block_neodymium);
                 }
             }
